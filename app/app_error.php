b3c986e7d32e011d48cea7729c0ceb8a6ca77264
Robert Wiley <ylee@bodhilinux.com>
2012-04-23 10:33:15 -0400

    Fix for not found error on search queries > 3.

--- app/app_error.php
+++ app/app_error.php
@@ -1,11 +1,18 @@
 <?PHP
-class AppError extends ErrorHandler {
- 	var $helpers = array('Html', 'Session','Javascript', 'Ajax');
-    function oopsError($params) {
-    $this->controller->set('page', $params['page']);
-    $this->_outputMessage('oopsError');
-    }
- 
+class AppError extends ErrorHandler
+{
+	var $helpers = array('Html', 'Session','Javascript', 'Ajax');
+	function oopsError($params)
+	{
+	$this->controller->set('page', $params['page']);
+	$this->_outputMessage('oopsError');
+	}
+	function searchError($params)
+	{
+	$this->controller->set('query', $params['query']);
+	$this->_outputMessage('searchError');
+	}
+
 }
 ?>
 
