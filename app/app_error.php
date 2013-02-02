881cee3dbcbf1b8c1105db377ff65ad03cc479d9
Robert Wiley <ylee@bodhilinux.com>
2013-02-02 16:57:14 -0500

    Dealing with url errors for production mode

--- app/app_error.php
+++ app/app_error.php
@@ -2,11 +2,17 @@
 class AppError extends ErrorHandler {
  	var $helpers = array('Html', 'Session','Javascript', 'Ajax');
     function oopsError($params) {
-    $this->controller->set('page', $params['page']);
-    $this->controller->set('archTypeDBList',$params['archTypeDBList']);
-    $this->_outputMessage('oopsError');
+		$this->controller->set('page', $params['page']);
+		$this->controller->set('archTypeDBList',$params['archTypeDBList']);
+		$this->_outputMessage('oopsError');
     }
- 
+    function error404($params){
+		$this->controller->set('url', $params['url']);
+		$this->controller->set('base', $params['base']);
+		# archTypeDb bs here is not working ignoring for now
+		$this->controller->set('archTypeDBList',$params['archTypeDBList']);
+		$this->_outputMessage('oopsError');
+	}
 }
 ?>
 
--- app/views/errors/oops_error.ctp
+++ app/views/errors/oops_error.ctp
@@ -2,5 +2,8 @@
 <?PHP 
 echo "<img class='opps' src='/img/404.png' alt=' ' />";
 echo "<br />";
-echo "<h2 style='text-align: center;'>Error code to report:".base64_encode($page)."</h2>";?>
+if (isset($page)){
+	echo "<h2 style='text-align: center;'>Error code to report:".base64_encode($page)."</h2>";
+}
+?>
 <br />
